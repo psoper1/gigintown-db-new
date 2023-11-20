937c874bcd5033c3a34356ead56bfe37c00aec73
psoper1 <psoper0604@gmail.com>
2023-11-20 11:27:03 -0500

    changed auth token lifetime to 15 minutes

--- gigintowndb/gigintowndb/settings.py
+++ gigintowndb/gigintowndb/settings.py
@@ -53,8 +53,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
     'ALGORITHM': 'HS256',
